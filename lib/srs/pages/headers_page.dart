b9ca462344f87ef4363e3f134bd7bac151bb01bc
Alejandro Pinedo B <apinedobarrios@live.com>
2021-08-18 00:45:25 -0500

    Se realizo el diseño del header con separación cuadrada en la parte superior

---
+++ lib/srs/pages/headers_page.dart
@@ -0,0 +1,10 @@
+import 'package:designs_app/srs/widgets/headers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: HeaderCuadrado());
+  }
+}
---
+++ lib/srs/widgets/headers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(
+          0xff615AAB), //Colocación de un color hex, se coloca 0xff seguida del color
+    );
+  }
+}
